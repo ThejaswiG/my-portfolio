31516cfc48b7790096cc882972b86cbe342b5d38
ThejaswiG <63565875+ThejaswiG@users.noreply.github.com>
2025-04-26 14:04:47 -0500

    Portfolio

---
+++ build/precache-manifest.ffca54ea866d0d652add60af1f81e8d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfcaf01cdff9fc69f42dc652b65d473",
+    "url": "/ThejaswiG/my-portfolio/index.html"
+  },
+  {
+    "revision": "6a38623718dc7663c63c",
+    "url": "/ThejaswiG/my-portfolio/static/css/main.db3b8e08.chunk.css"
+  },
+  {
+    "revision": "c0b3823ee426bde0e8cf",
+    "url": "/ThejaswiG/my-portfolio/static/js/0.5b7f18c3.chunk.js"
+  },
+  {
+    "revision": "2cb81322ece1125976c3",
+    "url": "/ThejaswiG/my-portfolio/static/js/3.c7206c53.chunk.js"
+  },
+  {
+    "revision": "6a38623718dc7663c63c",
+    "url": "/ThejaswiG/my-portfolio/static/js/main.7fce712a.chunk.js"
+  },
+  {
+    "revision": "88308f3ced61f01cd985",
+    "url": "/ThejaswiG/my-portfolio/static/js/runtime~main.7c003302.js"
+  },
+  {
+    "revision": "a3989b1b9862ade37537ee6631677639",
+    "url": "/ThejaswiG/my-portfolio/static/media/ViewAllCardImg.a3989b1b.png"
+  },
+  {
+    "revision": "8e36b51d5ee6b04e6d8fc65d622d7a65",
+    "url": "/ThejaswiG/my-portfolio/static/media/profile_pic-1.8e36b51d.jpg"
+  }
+]);
